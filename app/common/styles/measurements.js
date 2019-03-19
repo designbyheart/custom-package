127cd218ebbcf2c2844f6ae43d532600d9ce7bfb
Aleksandar Markovic <aleksandar.markovic@evernym.com>
2019-03-19 17:07:21 +0100

    clean measurements.js

--- app/common/styles/measurements.js
+++ app/common/styles/measurements.js
@@ -1,15 +1,15 @@
 // @flow
 
-import { Dimensions, PixelRatio, Platform } from 'react-native'
+import { Dimensions, Platform } from 'react-native'
 import { iPhoneXHeight } from './constant'
 
 const windowSize = Dimensions.get('window')
-const WINDOW_WIDTH = windowSize.width
 const WINDOW_HEIGHT = windowSize.height
 
-//default values for connect me
+//default nav bar height value for connect me
 const bottomNavBarHeight = 50
 let bottomBlurNavBarHeight
+//Ios responsive calculation for blur effect, we dont use blur effect on android
 switch (WINDOW_HEIGHT) {
   case iPhoneXHeight:
     bottomBlurNavBarHeight = 82
@@ -19,8 +19,6 @@ switch (WINDOW_HEIGHT) {
 }
 
 export const measurements = {
-  WINDOW_WIDTH: WINDOW_WIDTH,
-  WINDOW_HEIGHT: WINDOW_HEIGHT,
   bottomNavBarHeight,
   bottomBlurNavBarHeight,
 }
