0552aa9080bca9c09e5297cc5933b520b163f47c
Bogdan Boksan <bogdan.boksan@evernym.com>
2020-05-01 11:17:26 +0000

    Fixed modal header to accommodate two lines of text for credential and proof name

--- app/connection-details/components/modal-header.js
+++ app/connection-details/components/modal-header.js
@@ -14,35 +14,28 @@ import { CheckmarkBadge } from '../../components/connection-details/checkmark-ba
 // TODO: Fix the <any, void> to be the correct types for props and state
 class ModalHeader extends PureComponent<any, void> {
   render() {
-    const { useColorPicker = false } = this.props
+    const { institutialName, credentialName, credentialText, imageUrl, colorBackground } = this.props
 
     return (
       <View style={styles.container}>
-        <View style={[styles.row, { paddingBottom: 15 }]}>
-          <View style={styles.icon}>
+        <View style={styles.topSection}>
+          <View style={styles.imageSection}>
             <Image
-              style={styles.iconImage}
-              source={{ uri: this.props.imageUrl }}
-              resizeMode={'cover'}
+              style={styles.image}
+              source={{ uri: imageUrl }}
+              resizeMode="cover"
             />
           </View>
-          <View style={styles.header}>
-            <Text style={styles.name}>{this.props.institutialName}</Text>
-            <Text style={styles.title}>{this.props.credentialText}</Text>
+          <View style={styles.issuerAndInfoSection}>
+            <Text style={styles.issuerNameText}>{institutialName}</Text>
+            <Text style={styles.infoText}>{credentialText}</Text>
           </View>
-          <View style={styles.checkmark}>
-            {/* <Image style={styles.checkmarkImage} source={require ('./src/checkmarkBadgeBlue.png')} /> */}
-            <CheckmarkBadge color={this.props.colorBackground} />
+          <View style={styles.checkmarkSection}>
+            <CheckmarkBadge color={colorBackground} />
           </View>
         </View>
-        <View style={styles.row}>
-          <View style={styles.header}>
-            <Text style={styles.mainTitle}>{this.props.credentialName}</Text>
-            {/* <Text style={styles.secured}>Secured By Sovrin</Text> */}
-          </View>
-          {/* <TouchableOpacity style={styles.share}>
-                        <Image style={styles.shareImage} source={require ('./src/shareIcon.png')} />
-                    </TouchableOpacity> */}
+        <View style={styles.bottomSection}>
+          <Text numberOfLines={2} ellipsizeMode='tail' style={styles.credentialProofQuestionText}>{credentialName}</Text>
         </View>
       </View>
     )
@@ -56,8 +49,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     flexDirection: 'column',
     width: '100%',
-    height: 112,
-    padding: 17,
+    height: 114,
     shadowColor: '#000000',
     shadowOpacity: 0.1,
     shadowRadius: 14,
@@ -67,71 +59,60 @@ const styles = StyleSheet.create({
     },
     elevation: Platform.OS === 'android' ? 8 : 0,
   },
-  row: {
+  topSection: {
     flexDirection: 'row',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-    width: '100%',
+    height: '50%',
+    width: '100%'
   },
-  icon: {
-    width: 45,
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-    flexDirection: 'column',
+  imageSection: {
+    height: '100%',
+    width: 64,
+    paddingTop: 16,
+    paddingLeft: 16
   },
-  iconImage: {
-    width: 32,
-    height: 32,
-  },
-  header: {
+  issuerAndInfoSection: {
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
+    paddingTop: 10
   },
-  name: {
+  checkmarkSection: {
+    height: '100%',
+    width: 64,
+    alignItems: 'flex-end',
+    paddingRight: 16,
+    paddingTop: 16,
+  },
+  issuerNameText: {
     fontSize: 17,
     fontWeight: '700',
     color: '#505050',
     fontFamily: 'Lato',
   },
-  title: {
+  infoText: {
     fontSize: 14,
     fontWeight: '400',
     color: '#777',
     fontFamily: 'Lato',
   },
-  mainTitle: {
-    fontSize: 22,
-    fontWeight: '400',
-    color: '#505050',
-    fontFamily: 'Lato',
-  },
-  secured: {
-    fontSize: 14,
-    fontWeight: '400',
-    color: '#777',
-    fontFamily: 'Lato',
+  image: {
+    width: 32,
+    height: 32
   },
   checkmark: {
-    width: 45,
-    alignItems: 'flex-end',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    backgroundColor: 'white',
-  },
-  checkmarkImage: {
     width: 23,
     height: 34.5,
   },
-  share: {
-    width: 45,
-    alignItems: 'flex-end',
-    justifyContent: 'center',
-    flexDirection: 'column',
+  bottomSection: {
+    height: '50%',
+    width: '100%',
+    paddingLeft: 16,
+    paddingRight: 16,
+    justifyContent: 'center'
   },
-  shareImage: {
-    width: 24,
-    height: 26,
-  },
+  credentialProofQuestionText: {
+    fontSize: 21,
+    fontWeight: '400',
+    color: '#505050',
+    fontFamily: 'Lato',
+    marginBottom: 4
+  }
 })
