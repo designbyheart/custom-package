38bdf5488c81b8ff7556dec77a2581c3e2a74dc0
Vladimir Shishkin <vladimir.shishkin@evernym.com>
2020-07-09 11:17:08 +0000

    update screenshots and increase threshold
    Signed-off-by: Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>

--- e2e/utils/screenshot.js
+++ e2e/utils/screenshot.js
@@ -133,7 +133,7 @@ const areSameNew = (image1: string, image2: string, diffImagePath: string) => {
   const delta = new PNG({ width, height })
 
   let res = pixelmatch(img1.data, img2.data, delta.data, width, height, {
-    threshold: 0.7,
+    threshold: 1,
   })
 
   fs.writeFileSync(diffImagePath, PNG.sync.write(delta))
