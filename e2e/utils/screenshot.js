db42db99862ab7e439105770d21c7d0d3f5bd64d
Vladimir Shishkin <vladimir.shishkin@evernym.com>
2020-08-21 14:57:38 +0000

    Update screenshot.js

--- e2e/utils/screenshot.js
+++ e2e/utils/screenshot.js
@@ -155,7 +155,7 @@ const areSameNew = (image1: string, image2: string, diffImagePath: string) => {
   const delta = new PNG({ width, height })
 
   let res = pixelmatch(img1.data, img2.data, delta.data, width, height, {
-    threshold: 0.7,
+    threshold: 0.75,
   })
 
   fs.writeFileSync(diffImagePath, PNG.sync.write(delta))
